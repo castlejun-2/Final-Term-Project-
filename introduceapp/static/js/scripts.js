feee2c20ee723ecf821189492980271030121899
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-06-20 18:38:28 +0900

    Update Css, Js File

--- introduceapp/static/js/scripts.js
+++ introduceapp/static/js/scripts.js
@@ -1,13 +1,54 @@
-document.getElementById('project').onclick = function () {
-  document.getElementsByClassName('container px-4 px-lg-5 main')[0].style.display = 'none';
-  document.getElementsByClassName('container px-4 px-lg-5 project')[0].style.display = 'block';
-  document.getElementById('home').classList.remove('active');
-  document.getElementById('project').classList.add('active');
-};
+/*!
+* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
 
-document.getElementById('home').onclick = function () {
-  document.getElementsByClassName('container px-4 px-lg-5 project')[0].style.display = 'none';
-  document.getElementsByClassName('container px-4 px-lg-5 main')[0].style.display = 'block';
-  document.getElementById('project').classList.remove('active');
-  document.getElementById('home').classList.add('active');
-};
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
