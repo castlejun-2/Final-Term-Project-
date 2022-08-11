b4cff3fcca1e244230e845062cf410226a78611a
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-08-12 04:13:23 +0900

    방명록 등록일 한국시간 설정

--- introduceapp/models.py
+++ introduceapp/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from datetime import date, datetime, timezone, timedelta
-KST = timezone(timedelta(hours=9))
 
 # Create your models here.
 class GuestBook(models.Model):
     name = models.CharField(max_length=30, default="익명")
     contents = models.TextField()
-    cdate = models.DateTimeField(default=datetime.now(KST))
+    cdate = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
