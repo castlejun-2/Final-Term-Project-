4d816f2b026a701ff693c12f7a6485963290b15a
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-08-11 22:18:50 +0900

    DB 모델명 변경

--- introduceapp/views.py
+++ introduceapp/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import redirect, render
-from guestBook import *
+from introduceapp.models import GuestBook
 
 # Create your views here.
 def index(request):
-    guestBooks = GeustBook.objects.all().order_by('-cdate')[:3]        
+    guestBooks = GuestBook.objects.all().order_by('-cdate')[:3]        
     return render(request, 'introduceapp/index.html', {'guestBooks': guestBooks})
 
 def createGuestBook(request):
     if request.method == 'POST':
-        guestBook = GeustBook()
+        guestBook = GuestBook()
         guestBook.name = request.POST['name']
         guestBook.content = request.POST['content']
         guestBook.save()
