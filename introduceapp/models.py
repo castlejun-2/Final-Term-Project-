7664225cf5bb21c24482eac0fc5e6407c69d703c
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-08-11 19:26:00 +0900

    방명록을 위한 DB 모델 생성

--- introduceapp/models.py
+++ introduceapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class GeustBook(models.Model):
+    name = models.CharField(max_length=30)
+    contents = models.TextField()
+    cdate = models.DateTimeField(auto_now_add=True)
