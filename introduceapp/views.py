abcb5773d348d9c76da526e9024ddff79252c838
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-08-11 22:03:59 +0900

    방명록 내용을 받기 위한 값 생성

--- introduceapp/views.py
+++ introduceapp/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from guestBook import *
 
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
-        guestBook = Form(request.POST)
-        if guestBook.is_valid():
-            guestBook.save()
     guestBooks = GeustBook.objects.all().order_by('-cdate')[:3]        
     return render(request, 'introduceapp/index.html', {'guestBooks': guestBooks})
+
+def createGuestBook(request):
+    if request.method == 'POST':
+        guestBook = Form()
+        guestBook.name = request.POST['name']
+        guestBook.content = request.POST['content']
+        guestBook.save()
+    return redirect('index')
