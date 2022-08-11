2b08709d0a5c427ad05be1e288468f9ea5c0e300
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-08-11 19:51:33 +0900

    방명록 Form 전송

--- introduceapp/views.py
+++ introduceapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from guestBook import *
+import logging
 
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        form = Form()
+        logging.debug(form)
     return render(request, 'introduceapp/index.html')
